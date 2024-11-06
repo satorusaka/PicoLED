d6ea877a9851f726ef9bd349347d8e4aa2f2b029
satorusaka <40286047+satorusaka@users.noreply.github.com>
2024-11-06 19:38:03 +0800

    consistent formatting

--- WS2812B.cpp
+++ WS2812B.cpp
@@ -1,23 +1,19 @@
 #include "WS2812B.hpp"
 #include "WS2812B.pio.h"
 
-namespace PicoLed
-{
+namespace PicoLed {
 
-    WS2812B::WS2812B(PIO pioBlock, uint stateMachine, uint dataPin, uint numLeds, DataByte b1, DataByte b2, DataByte b3, DataByte b4, bool isInverted) : PioStrip(pioBlock, stateMachine, dataPin, numLeds, b1, b2, b3, b4, isInverted)
-    {
-        uint bits = (b4 == NONE ? 24 : 32);
+WS2812B::WS2812B(PIO pioBlock, uint stateMachine, uint dataPin, uint numLeds, DataByte b1, DataByte b2, DataByte b3, DataByte b4, bool isInverted):
+PioStrip(pioBlock, stateMachine, dataPin, numLeds, b1, b2, b3, b4, isInverted) {
+    uint bits = (b4 == NONE ? 24 : 32);
 
-        if (isInverted)
-        {
-            uint offset = pio_add_program(pioBlock, &WS2812B_inverted_program);
-            WS2812B_inverted_program_init(pioBlock, stateMachine, offset, dataPin, 800000, bits);
-        }
-        else
-        {
-            uint offset = pio_add_program(pioBlock, &WS2812B_program);
-            WS2812B_program_init(pioBlock, stateMachine, offset, dataPin, 800000, bits);
-        }
+    if (isInverted) {
+        uint offset = pio_add_program(pioBlock, &WS2812B_inverted_program);
+        WS2812B_inverted_program_init(pioBlock, stateMachine, offset, dataPin, 800000, bits);
+    } else {
+        uint offset = pio_add_program(pioBlock, &WS2812B_program);
+        WS2812B_program_init(pioBlock, stateMachine, offset, dataPin, 800000, bits);
     }
-
 }
+
+}  // namespace PicoLed
